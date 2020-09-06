9e160701096b7c51111d0167490e184b151ce164
Cream Crumpets <fluffy1780@me.com>
2020-09-06 14:28:37 -0400

    Update user agent

--- BingBot.py
+++ BingBot.py
@@ -68,7 +68,7 @@ def login(b):
     login.click() # Submit password
     return b
 
-b = Browser("chrome", user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240")
+b = Browser("chrome", user_agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4254.2 Safari/537.36")
 b = login(b)
 sleep(1)
 # Perform random number of desktop searches.
